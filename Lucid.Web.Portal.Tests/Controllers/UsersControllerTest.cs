e412aab9306fe0cd183c3e94f2f3562b658e4260
James Abney <=>
2013-06-19 13:17:09 -0700

    Dbcontext
    Added SQL Server enabled dbcontext configuration with first user model

---
+++ Lucid.Web.Portal.Tests/Controllers/UsersControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lucid.Web.Portal.Tests.Controllers
+{
+    [TestClass]
+    public class UsersControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Lucid.Web.Portal/Models/LucidWebPortalContext.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Lucid.Web.Portal.Models
+{
+    public class LucidWebPortalContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, add the following
+        // code to the Application_Start method in your Global.asax file.
+        // Note: this will destroy and re-create your database with every model change.
+        // 
+        public LucidWebPortalContext()
+        {
+            System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<Lucid.Web.Portal.Models.LucidWebPortalContext>());
+        }
+
+        public DbSet<Lucid.Web.Portal.Models.User> Users { get; set; }
+    }
+}
