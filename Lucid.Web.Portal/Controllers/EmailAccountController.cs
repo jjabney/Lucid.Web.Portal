6ee041c8f1345ae830eeba763085089953ce9c9b
James Abney <=>
2013-06-18 20:14:01 -0700

    Added EmailAccountController

---
+++ Lucid.Web.Portal/Controllers/EmailAccountController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Lucid.Web.Portal.Controllers
+{
+    public class EmailAccountController : ApiController
+    {
+        // GET api/emailaccount
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/emailaccount/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/emailaccount
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/emailaccount/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/emailaccount/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
