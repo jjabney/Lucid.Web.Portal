31de42c01524a2dd99da5169753c6ab1b12e6da6
James Abney <jjabney@gmail.com>
2013-07-19 19:53:22 -0700

    Changed commit

--- Lucid.Web.Portal/Models/LucidWebPortalContext.cs
+++ Lucid.Web.Portal/Models/LucidWebPortalContext.cs
@@ -17,7 +17,7 @@ namespace Lucid.Web.Portal.Models
         // 
         public LucidWebPortalContext()
         {
-            System.Data.Entity.Database.SetInitializer(new System.Data.Entity.DropCreateDatabaseIfModelChanges<LucidWebPortalContext>());
+            System.Data.Entity.Database.SetInitializer(new System.Data.Entity.CreateDatabaseIfNotExists<LucidWebPortalContext>());
         }
 
    
